1b4a76050b6486e2fed47325ef6ed796a8857c3c
Xin Zhe Khooi <khooi8913@gmail.com>
2021-07-26 02:49:54 +0000

    one script for all

---
+++ generate_pcap.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+function show_help
+{
+ echo -e "This script loads the (reduced) memcached trace keys and generates a PCAP."
+ echo -e "Example: ./generate_pcap.sh -i <INPUT_DIRECTORY> -o <OUTPUT_DIRECTORY>"
+ echo -e "\t\t-i <INPUT_DIRECTORY>: Directory containing the (reduced) memcached trace keys."
+ echo -e "\t\t-o <OUTPUT_DIRECTORY>: Directory to contain the resulting PCAP files. Cannot be the same with INPUT_DIRECTORY."
+ exit
+ }
+
+while getopts "h?i:o:" opt
+ do
+ 	case "$opt" in
+ 	h|\?)
+ 		show_help
+ 		;;
+ 	i)
+ 		input_directory=$OPTARG
+ 		;;
+ 	o)
+ 		output_directory=$OPTARG
+ 		;;
+ 	*)
+ 		show_help
+ 		;;
+ 	esac
+done
+
+if [ -z $input_directory ]
+then
+  echo -e "No INPUT_DIRECTORY is set!"
+  show_help
+fi
+if [ -z $output_directory ]
+then
+  echo -e "No OUTPUT_DIRECTORY is set!"
+  show_help
+fi
+
+if [ $input_directory == $output_directory ]
+then 
+	echo -e "INPUT_DIRECTORY cannot be the same with OUTPUT_DIRECTORY!"
+fi
+
+if [ ! -d $output_directory ] 
+then
+	mkdir $output_directory
+fi
+
+traces=`ls $input_directory`
+# completed=`ls $output_directory`
+# completed=`echo $completed | sed 's/.pcap//g'`	
+
+for trace in $traces
+do 
+	echo "Currently processing $input_directory/$trace ..."
+	awk '{ printf "payload=0001%08x00000000\n", $1 }' $input_directory/$trace > /tmp/tmp.csv
+	./pcap_generator_from_csv.py -i /tmp/tmp.csv -o $output_directory/$trace > /dev/null	
+done
+
+
+
+# for i in $(cat tmp/sample.out); do printf 'payload=0001%08x00000000\n' $i >> tmp.csv; done
